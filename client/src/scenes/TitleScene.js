eea899641b880d064f3aea3958368c14555cc9f2
monika.wiszmann <monika.wiszmann@gmail.com>
2019-06-15 19:40:30 +0200

    Change background color

--- client/src/scenes/TitleScene.js
+++ client/src/scenes/TitleScene.js
@@ -41,12 +41,30 @@ class TitleScene extends Phaser.Scene {
         el.style.width = 400 * multiplier + 'px';
         el.style.height = 240 * multiplier + 'px';
 
+        //cat and mouse to choose character
+        this.mouseSprite = this.add.sprite(16 * 8 + 4, 4 * 16, "atlas", "mouse_down");
+        this.catSprite = this.add.sprite(16 * 16 + 4, 4 * 16, "atlas", "cat_down");
+        this.characterType = "";
+        this.selectMouse(true);
+
+        /*
+        // highlite selection, but circle does not update
+        this.mouseSprite.setDepth(10);
+        this.catSprite.setDepth(10);
+        this.graphics = this.add.graphics({ fillStyle: { color: 0xffffff } });
+        this.circle = new Phaser.Geom.Circle(this.mouseSprite.x, this.mouseSprite.y, 16);
+        this.graphics.fillCircleShape(this.circle);*/
+
+        this.leftKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        this.rightKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+
         this.pressX = this.add.bitmapText(16 * 8 + 4, 8 * 16, 'font', 'PRESS X TO START', 8);
+
+        //todo get all players and add text
+
         this.blink = 1000;
 
         this.startKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.X);
-        this.check = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-
     }
 
     update(time, delta) {
@@ -59,18 +77,38 @@ class TitleScene extends Phaser.Scene {
             this.blink = 500;
         }
 
+        if(this.leftKey.isDown) {
+            this.selectMouse(true);
+        }
+
+        if(this.rightKey.isDown) {
+            this.selectMouse(false);
+        }
+
         if (this.startKey.isDown) {
             this.startGame();
         }
+    }
 
-        if (this.check.isDown) {
-            this.client.addPlayer();
+    selectMouse(isMouse) {
+        if (isMouse) {
+            this.mouseSprite.anims.play("mouse_down");
+            this.catSprite.anims.stop();
+            //this.circle = this.circle.setTo(this.mouseSprite.x, this.mouseSprite.y, 18);
+            this.characterType = "mouse"
         }
+        else {
+            this.catSprite.anims.play("cat_down");
+            this.mouseSprite.anims.stop();
+            //this.circle = this.circle.setTo(this.catSprite.x, this.catSprite.y, 18);
+            this.characterType = "cat";
+        }
+
     }
 
     startGame() {
-       // this.scene.stop('GameScene');
         this.scene.start('GameScene');
+        //todo create player and send to server
     }
 
     restartScene() {
