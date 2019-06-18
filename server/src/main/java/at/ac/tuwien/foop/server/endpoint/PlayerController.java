da3017fd85c51a75bfb02c72c52f9d0fee2b059f
monika.wiszmann <monika.wiszmann@gmail.com>
2019-06-18 09:32:27 +0200

    update addPlayer method

--- server/src/main/java/at/ac/tuwien/foop/server/endpoint/PlayerController.java
+++ server/src/main/java/at/ac/tuwien/foop/server/endpoint/PlayerController.java
@@ -1,28 +1,29 @@
 package at.ac.tuwien.foop.server.endpoint;
 
 
+import at.ac.tuwien.foop.server.dto.LoginResponse;
 import at.ac.tuwien.foop.server.dto.Response;
+import at.ac.tuwien.foop.server.service.PrepareService;
+import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
-import static at.ac.tuwien.foop.server.util.PlayerIdCache.getPlayerId;
-
 @Controller
+@AllArgsConstructor
 public class PlayerController {
 
     private static final Logger LOG = LoggerFactory.getLogger(PlayerController.class);
 
-    @MessageMapping(value = "/addPlayer")
-    @SendTo("/topic/addPlayer")
-    public Response addPlayer(String sessionId) {
-        Long playerId = getPlayerId(sessionId);
-        LOG.info("Adding Player {}", playerId);
-        return Response.builder()
-                .message("Player" + playerId + " joined the Game!")
-                .build();
+    private final PrepareService prepareService;
+
+    @MessageMapping("/addPlayer")
+    @SendTo("/topic/player")
+    public LoginResponse addPlayer(String name) {
+        LOG.info("Creating player " + name);
+        return prepareService.createPlayer(name);
     }
 
 }
